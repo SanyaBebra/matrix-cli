444199653e94f4b85de8e34d96de7923c1cd4fbb
SanyaGrozniy <vasabot228@gmail.com>
2025-11-01 14:52:11 +0200

    Version 1.1: Добавлена функция транспортировки матрицы; исправлены ошибки

--- matrix.cpp
+++ matrix.cpp
@@ -4,38 +4,33 @@
 using namespace std;
 
 // История всех матиц за время работы программы
-vector<  vector<vector<int>>  > matrix_history; 
+vector<  vector<vector<int>>  > matrices_history; 
 
 // Вывод меню
-void view_menu() {
+void show_menu() {
   cout << "\n\n-========================-\n\n";
   cout << "\t  Menu\n" << endl;
   cout << "1 - Add a matrix" << endl;
-  cout << "2 - View all matrix" << endl;
+  cout << "2 - Show all matrices" << endl;
+  cout << "3 - Trasport the matrix" << endl;
   cout << "\n\n-========================-\n\n";
 }
 
-// Вывод матрицу
-void view_matrix(const vector<vector<int>> &matrix) {
+// Вывод матрицы
+void show_matrix(const vector<vector<int>> &matrix) {
   for (int i = 0; i < matrix.size(); i++) {
     for (int j = 0; j < matrix.size(); j++) 
       cout << matrix[i][j] << '\t';
 
-    cout << endl;
+    cout << "\n\n";
   }
 }
 
-void view_all_matrix(const vector< vector<vector<int>> > &history) {
+// Вывод всех матриц
+void show_all_matrices(const vector< vector<vector<int>> > &history) {
   for (int i = 0; i < history.size(); i++) {
     cout << "Index " << i << ":" << endl;
-    for (int row = 0; row < history[i].size(); row++) {
-
-      for (int col = 0; col < history[i][row].size(); col++) 
-        cout << history[i][row][col] << '\t';
-
-      cout << "\n\n";
-    }
-    
+    show_matrix(history[i]);
   }
 }
 
@@ -52,11 +47,20 @@ vector< vector<int> > create_matrix(int size) {
     return matrix; // Возвращаем готовую матрицу
 }
 
+// Транспоитрует матрицу
+void transpose_matrix(vector< vector<int> > &matrix) {
+  vector<vector<int>> transpose = matrix;
+
+  for (int i = 0; i < matrix.size(); i++) 
+    for (int j = 0; j < matrix.size(); j++) 
+      matrix[i][j] = transpose[j][i];
+}
+
 int main() {
-  cout << "Версия " << 1.0 << endl;
+  cout << "Version " << 1.1 << endl;
 
   cout << "Matrix" << endl;
-  view_menu(); // Вывод меню
+  show_menu(); // Вывод меню
 
   int option = 0;
   cout << "Option: ";
@@ -67,10 +71,19 @@ int main() {
       cin >> size;
 
       vector< vector<int> > matrix = create_matrix(size); 
-      matrix_history.push_back(matrix);
-      view_matrix(matrix);
+      matrices_history.push_back(matrix);
+      show_matrix(matrix);
     } else if (option == 2) {
-      view_all_matrix(matrix_history);
+      show_all_matrices(matrices_history);
+    } else if (option == 3) {
+      if (matrices_history.size() != 0) { // Выполнять если вектор не пустой
+        // Выбрать матрицу
+        cout << "Change a matrix by index: ";
+        int matrix_index = 0;
+        cin >> matrix_index;
+
+        transpose_matrix(matrices_history[matrix_index]); // Транспортируем матрицу
+      }
     }
 
     cout << "Option: ";
